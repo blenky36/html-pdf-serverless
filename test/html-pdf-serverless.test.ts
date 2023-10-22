9c90e13cb49df38124e5b255a5ddb3d6411decdd
Phil Blenkinsop <philblenkinsop@Phils-MacBook-Pro-13.local>
2023-10-22 20:24:40 +0100

    Initial commit

---
+++ test/html-pdf-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HtmlPdfServerless from '../lib/html-pdf-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/html-pdf-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HtmlPdfServerless.HtmlPdfServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
